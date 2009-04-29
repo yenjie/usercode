6e1f4bef0076615e8e2fa7bb958c7de83598a120
yjlee <>
2009-04-29 16:55:50 +0000

    *** empty log message ***

--- RecoHIEgamma/HICaloDisplayer/src/HICaloDisplayer.cc
+++ RecoHIEgamma/HICaloDisplayer/src/HICaloDisplayer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Yen-Jie Lee
 //         Created:  Wed Jun 18 20:21:55 EDT 2008
-// $Id$
+// $Id: HICaloDisplayer.cc,v 1.3 2009/03/17 17:25:25 yjlee Exp $
 //
 //
 
@@ -30,10 +30,13 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+
 #include "DataFormats/Math/interface/Vector3D.h"
 #include "RecoHIEgamma/HIEgammaTools/interface/CxCalculator.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "Geometry/Records/interface/IdealGeometryRecord.h"
+
 #include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/EgammaReco/interface/BasicCluster.h"
@@ -44,18 +47,44 @@
 #include "DataFormats/Candidate/interface/Candidate.h"
 
 #include "Geometry/CaloGeometry/interface/CaloGeometry.h"
-#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
 #include "RecoCaloTools/MetaCollections/interface/CaloRecHitMetaCollections.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHit.h"
 
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 
+#include "TTree.h"
 #include "TNtuple.h"
 #include "TFile.h"
 
 //
 // class decleration
 //
+
+
+struct MyEventContent{
+   int nbc;
+   int nhhit;
+   int nehit;
+   int ngen;
+   float bcEta[10000];
+   float bcPhi[10000];
+   float bcE[10000];
+   float ehitEta[10000];
+   float ehitPhi[10000];
+   float ehitE[10000];
+   float hhitEta[10000];
+   float hhitPhi[10000];
+   float hhitE[10000];
+   float gPhi[10000];
+   float gEta[10000];
+   float gE[10000];
+   float gId[10000];
+   float gMid[10000];
+   float gS[10000];
+};
 
 class HICaloDisplayer : public edm::EDAnalyzer {
    public:
@@ -76,12 +105,12 @@ class HICaloDisplayer : public edm::EDAnalyzer {
    const EcalRecHitCollection   *fEBHit_;
    const EcalRecHitCollection   *fEEHit_;
   
-   TNtuple *datatemp;
-   TNtuple *datatemp1;
-   TNtuple *datatemp2;
-   TNtuple *datatemp3;
-   TFile *f;
+   edm::Service<TFileService> fs;           
+   TTree* EventTree_;
+
    int nEvent;
+   MyEventContent pev_;
+   
    std::string GenCandInput;
    std::string output;
 
@@ -102,7 +131,7 @@ HICaloDisplayer::HICaloDisplayer(const edm::ParameterSet& iConfig)
 
 {
    //now do what ever initialization is needed
-   GenCandInput = iConfig.getUntrackedParameter<std::string>("GenCandInput", "genParticleCandidates");
+   GenCandInput = iConfig.getUntrackedParameter<std::string>("GenCandInput","genParticles");
    output = iConfig.getUntrackedParameter<std::string>("output", "ughuu.root");
 
 }
@@ -138,16 +167,16 @@ HICaloDisplayer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
    fEEclusters_ = pEEclusters.product(); 
 
    ESHandle<CaloGeometry> geometryHandle;
-   iSetup.get<IdealGeometryRecord>().get(geometryHandle);
+   iSetup.get<CaloGeometryRecord>().get(geometryHandle);
    geometry_ = geometryHandle.product();
 
    Handle<HBHERecHitCollection> hehbhandle;
    iEvent.getByLabel(InputTag("hbhereco"), hehbhandle);
    fHBHERecHits_ = hehbhandle.product();
 
-   Handle<CandidateCollection> pGenCandInput;
+   Handle<GenParticleCollection> pGenCandInput;
    iEvent.getByLabel(InputTag(GenCandInput), pGenCandInput);
-   const CandidateCollection *genParticles = pGenCandInput.product();
+   const GenParticleCollection *genParticles = pGenCandInput.product();
    if(genParticles == 0)
    {
       cout <<"***HIPhotonCandidateAna: GenParticleCandidate not found!"<<endl;
@@ -165,43 +194,36 @@ HICaloDisplayer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
 
 
 
-   double candeta=0;
-   double candphi=0;
 
-
+   pev_.ngen=0;
    for(int i = 0; i < (int) genParticles->size(); i++)
    {
      const Candidate &cand = (*genParticles)[i];
-      float var[10];
-//      if (cand.et()<20) continue;
-      var[0]=nEvent;
-      var[1]=cand.phi();
-      var[2]=cand.eta();
-      var[3]=cand.energy();
-      var[4]=cand.pdgId();
+      pev_.gPhi[pev_.ngen] = cand.phi();
+      pev_.gEta[pev_.ngen] = cand.eta();
+      pev_.gE  [pev_.ngen] = cand.energy();
+      pev_.gId [pev_.ngen] = cand.pdgId();
+
       float mid=0;
       if (cand.mother()!=0) mid=cand.mother()->pdgId();
-
-      var[5]=mid;
-      var[6]=cand.status();
-      datatemp2->Fill(var);      
+      pev_.gMid[pev_.ngen] = mid;
+      pev_.gS  [pev_.ngen] = cand.status();
+      pev_.ngen++;
    }
 
    nEvent++;
 
+   pev_.nbc = 0;
    for(BasicClusterCollection::const_iterator iclu = fEBclusters_->begin();
        iclu != fEBclusters_->end(); ++iclu) {
       const BasicCluster *clu = &(*iclu);
       math::XYZVector ClusPoint(clu->x(),clu->y(),clu->z());
       double eta = ClusPoint.eta();
       double phi = ClusPoint.phi();
-      float var[10];
-      var[0]=nEvent;
-      var[1]=phi;
-      var[2]=eta;
-      var[3]=clu->energy();
-      datatemp->Fill(var);
-
+      pev_.bcPhi[pev_.nbc] = phi;
+      pev_.bcEta[pev_.nbc] = eta;
+      pev_.bcE  [pev_.nbc] = clu->energy();
+      pev_.nbc++;
    }    
 
    for(BasicClusterCollection::const_iterator iclu = fEEclusters_->begin();
@@ -210,28 +232,25 @@ HICaloDisplayer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
       math::XYZVector ClusPoint(clu->x(),clu->y(),clu->z());
       double eta = ClusPoint.eta();
       double phi = ClusPoint.phi();
-      float var[10];
-      var[0]=nEvent;
-      var[1]=phi;
-      var[2]=eta;
-      var[3]=clu->energy();
-      datatemp->Fill(var);
+      pev_.bcPhi[pev_.nbc] = phi;
+      pev_.bcEta[pev_.nbc] = eta;
+      pev_.bcE  [pev_.nbc] = clu->energy();
+      pev_.nbc++;
    }
 
-
-
+   
+   pev_.nehit=0;
    for(size_t index = 0; index < fEBHit_->size(); index++) {
       const EcalRecHit &rechit = (*fEBHit_)[index];
       const DetId &detid = rechit.id();
       const GlobalPoint& hitpoint = geometry_->getPosition(detid);
       double eta = hitpoint.eta();
       double phi = hitpoint.phi();
-      float var[10];
-      var[0]=nEvent;
-      var[1]=phi;
-      var[2]=eta;
-      var[3]=rechit.energy();
-      datatemp3->Fill(var);
+
+      pev_.ehitPhi[pev_.nehit] = phi;
+      pev_.ehitEta[pev_.nehit] = eta;
+      pev_.ehitE  [pev_.nehit] = rechit.energy();
+      pev_.nehit++;
    }
 
 
@@ -241,39 +260,29 @@ HICaloDisplayer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
       const GlobalPoint& hitpoint = geometry_->getPosition(detid);
       double eta = hitpoint.eta();
       double phi = hitpoint.phi();
-      float var[10];
-      var[0]=nEvent;
-      var[1]=phi;
-      var[2]=eta;
-      var[3]=rechit.energy();
-      datatemp3->Fill(var);
+
+      pev_.ehitPhi[pev_.nehit] = phi;
+      pev_.ehitEta[pev_.nehit] = eta;
+      pev_.ehitE  [pev_.nehit] = rechit.energy();
+      pev_.nehit++;
    }
 
+   pev_.nhhit=0;
    for(size_t index = 0; index < fHBHERecHits_->size(); index++) {
       const HBHERecHit &rechit = (*fHBHERecHits_)[index];
       const DetId &detid = rechit.id();
       const GlobalPoint& hitpoint = geometry_->getPosition(detid);
       double eta = hitpoint.eta();
       double phi = hitpoint.phi();
-      float var[10];
-      var[0]=nEvent;
-      var[1]=phi;
-      var[2]=eta;
-      var[3]=rechit.energy();
-      datatemp1->Fill(var);
+
+      pev_.hhitPhi[pev_.nhhit] = phi;
+      pev_.hhitEta[pev_.nhhit] = eta;
+      pev_.hhitE  [pev_.nhhit] = rechit.energy();
+      pev_.nhhit++;
    }
 
-
-
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
+   EventTree_->Fill();
    
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
 }
 
 
@@ -282,37 +291,35 @@ void
 HICaloDisplayer::beginJob(const edm::EventSetup&)
 {
    TFile::TContext context(0);
-   f = TFile::Open(output.c_str(), "recreate");
-   datatemp = new TNtuple("bc", "ecal info",
-             "id:phi:eta:e"
-            );
 
-   datatemp1 = new TNtuple("hit", "hcal info",
-             "id:phi:eta:e"
-            );
+   EventTree_ = fs->make<TTree>("EventTree","Tree");
 
-   datatemp2 = new TNtuple("gen", "gen info",
-             "id:phi:eta:e:gid:mid:s"
-            );
-
-   datatemp3 = new TNtuple("ehit", "ecal info",
-             "id:phi:eta:e"
-            );
-
-  nEvent=0;
+   EventTree_->Branch("nbc",&pev_.nbc,"nbc/I");
+   EventTree_->Branch("nhhit",&pev_.nhhit,"nhhit/I");
+   EventTree_->Branch("nehit",&pev_.nehit,"nehit/I");
+   EventTree_->Branch("ngen",&pev_.ngen,"ngen/I");
+   EventTree_->Branch("bcEta",pev_.bcEta,"bcEta[nbc]/F");
+   EventTree_->Branch("bcPhi",pev_.bcPhi,"bcPhi[nbc]/F");
+   EventTree_->Branch("bcE",pev_.bcE,"bcE[nbc]/F");
+   EventTree_->Branch("hhitEta",pev_.hhitEta,"hhitEta[nhhit]/F");
+   EventTree_->Branch("hhitPhi",pev_.hhitPhi,"hhitPhi[nhhit]/F");
+   EventTree_->Branch("hhitE",pev_.hhitE,"hhitE[nhhit]/F");
+   EventTree_->Branch("ehitEta",pev_.ehitEta,"ehitEta[nehit]/F");
+   EventTree_->Branch("ehitPhi",pev_.ehitPhi,"ehitPhi[nehit]/F");
+   EventTree_->Branch("ehitE",pev_.ehitE,"ehitE[nehit]/F");
+   EventTree_->Branch("gPhi",pev_.gPhi,"gPhi[ngen]/F");
+   EventTree_->Branch("gEta",pev_.gEta,"gEta[ngen]/F");
+   EventTree_->Branch("gE",pev_.gE,"gE[ngen]/F");
+   EventTree_->Branch("gId",pev_.gId,"gId[ngen]/F");
+   EventTree_->Branch("gMid",pev_.gMid,"gMid[ngen]/F");
+   EventTree_->Branch("gS",pev_.gS,"gS[ngen]/F");
+   
+   nEvent=0;
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
 HICaloDisplayer::endJob() {
-   TFile::TContext context(f);
-
-   datatemp->Write();
-   datatemp1->Write();
-   datatemp2->Write();
-   datatemp3->Write();
-
-   f->Close();
 
 }
 
