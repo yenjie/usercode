6e2ccff40311d2d6e9f9531563bb9b63e3dfff00
yjlee <>
2009-05-06 16:54:26 +0000

    *** empty log message ***

--- RecoHIEgamma/HIEgammaTools/src/TxyCalculator.cc
+++ RecoHIEgamma/HIEgammaTools/src/TxyCalculator.cc
@@ -39,14 +39,26 @@ double TxyCalculator::getTxy(const reco::Candidate &candidate, double x, double 
    for(reco::TrackCollection::const_iterator
    	  recTrack = recCollection->begin(); recTrack!= recCollection->end(); recTrack++)
    {
+<<<<<<< TxyCalculator.cc
+      double pt = recTrack->pt();
+      double eta2 = recTrack->phi();
+      double phi2 = recTrack->phi();
+      
+      if(dRDistance(eta1,phi1,eta2,phi2) >= 0.1 * x)
+=======
       double pt = recTrack->pt();
       double eta2 = recTrack->eta();
       double phi2 = recTrack->phi();
       
       if(dRDistance(eta1,phi1,eta2,phi2) >= 0.1 * x)
+>>>>>>> 1.6
          continue;
 
+<<<<<<< TxyCalculator.cc
+      if(pt > y * 3)
+=======
       if(pt > y * 0.4)
+>>>>>>> 1.6
          txy = txy + 1;
    }
 
